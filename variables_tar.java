a4c4785846b0f585881fff84896bd10de8729548
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-11-23 20:29:59 +0200

    Update variables_tar.java

--- variables_tar.java
+++ variables_tar.java
@@ -8,6 +8,10 @@ import java.util.Scanner;
 
 // public = everyone can access
 // class Main
+
+/**
+ * This is my great Main class!
+ */
 public class Main
 {
     // public = everyone can access
@@ -16,7 +20,7 @@ public class Main
     // arg = 2 + 3
 
     /**
-     * ...java doc...
+     * This is my great main module!
      * @param args
      */
     public static void main(String[] args)
@@ -25,7 +29,6 @@ public class Main
        // moshe [israel.tel-aviv...]
         //Scanner s = new Scanner(System.in); // create input from keyboard
         //String my_name = s.nextLine(); // get input from keyboard
-
          //year = 2019; //Error!
         // this creates a variable of type int which is called "year"
         // X.123456789012345
@@ -50,5 +53,43 @@ public class Main
         // 4. *etgar: put in boolean the value of true
         // 5. print all the variables from 1-4
         // 6. add javadocs comment to the Main
+        short player_level = 1;
+        float today = 23.11f;
+        double number = 8.8876;
+        boolean flag = true;
+        boolean flag1 = false;
+        System.out.println(player_level);
+        System.out.println(today);
+        System.out.println(number);
+        System.out.println(flag);
+        System.out.println(flag1);
+
+        char letter = 'A';
+        System.out.println(letter);
+        System.out.println(letter + letter);
+
+        int x = 2,y = 3,z = 3;
+        //int r;
+        //System.out.println(r); // ERROR
+        System.out.println(x);
+        x = 12;
+        System.out.println(x);
+
+        //boolean b = true;
+        boolean b = (x > z); // true
+        boolean c = (1 > 2); // false
+
+        //boolean a1,b1,c1;
+        //System.out.println(a1);
+        
+        // Targil:
+        // 1. create an int variable which will have the value of:
+        //          785 * 231
+        // 2. create a float variable with the value of 4.3 * 7.8
+        // 3. create char named "lettx" with 'x'
+        // 4. create char named "letty" with 'y'
+        // 5. create in one line short xs,ys,zs
+        //      in this one line put in xs = 1 in ys = 2 in zs = 3
+        //    then in the next line put in xs the sum of ys and zs
     }
 }
