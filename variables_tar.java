3435494dfd52a31d65f53c2fbad6c92560100647
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-11-23 19:37:05 +0200

    Create variables_tar.java

---
+++ variables_tar.java
@@ -0,0 +1,54 @@
+// who ami I?
+// com = country, company = city
+package com.company;
+
+//     country.city.first_name
+//     israel.tel-aviv.moshe
+import java.util.Scanner;
+
+// public = everyone can access
+// class Main
+public class Main
+{
+    // public = everyone can access
+    // static = ...
+    // void = no return value. Send email ... void
+    // arg = 2 + 3
+
+    /**
+     * ...java doc...
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // /* */ /** java docs*/
+       // moshe [israel.tel-aviv...]
+        //Scanner s = new Scanner(System.in); // create input from keyboard
+        //String my_name = s.nextLine(); // get input from keyboard
+
+         //year = 2019; //Error!
+        // this creates a variable of type int which is called "year"
+        // X.123456789012345
+        // 3.141592653589793
+        int year = 2020;
+        year = 2019; //Sababa
+        float phone_size = 5.4f;
+        float size = 5;
+        double pi = 3.1415926535897932384626433;
+        System.out.println(size);
+        System.out.println(pi);
+
+        // Targil:
+        // 1.
+        // create a variable for integer numbers
+        //    which will not exceed +100, call it player_level
+        //    and put inside initial value of 1;
+        // 2.
+        // create a floating point value of the date today [month-day]
+        // [hint: 23.11]
+        // 3. put in double variable the value of : 8.8876
+        // 4. *etgar: put in boolean the value of true
+        // 5. print all the variables from 1-4
+        // 6. add javadocs comment to the Main
+    }
+}
