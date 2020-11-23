e2ee5b2f03271478b66b0dc0ec783361eb58426c
qa191120 <74743291+qa191120@users.noreply.github.com>
2020-11-23 21:40:44 +0200

    Update variables_tar.java

--- variables_tar.java
+++ variables_tar.java
@@ -82,14 +82,5 @@ public class Main
         //boolean a1,b1,c1;
         //System.out.println(a1);
         
-        // Targil:
-        // 1. create an int variable which will have the value of:
-        //          785 * 231
-        // 2. create a float variable with the value of 4.3 * 7.8
-        // 3. create char named "lettx" with 'x'
-        // 4. create char named "letty" with 'y'
-        // 5. create in one line short xs,ys,zs
-        //      in this one line put in xs = 1 in ys = 2 in zs = 3
-        //    *etgar: then in the next line put in xs the sum of ys and zs
     }
 }
